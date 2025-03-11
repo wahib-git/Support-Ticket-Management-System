c399abd30655e86b3093b08d5a6209d5a5c85771
wahib bachoua <wahibbachoua95@gmail.com>
2025-03-11 12:14:38 +0100

    config node mailer

---
+++ utils/mailer.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail', 
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+});
+
+
+const sendNotificationEmail = (to, subject, text) => {
+  const mailOptions = {
+    from: 'wahibbachoua95@gmail.com',
+    to,
+    subject,
+    text,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Erreur lors de l\'envoi de l\'email :', error);
+    } else {
+      console.log('Email envoyé avec succès :', info.response);
+    }
+  });
+};
+
+module.exports = sendNotificationEmail;
+
+
