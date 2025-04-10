536258d0dc83856fab346857a88aeac71796f218
wahib bachoua <wahibbachoua95@gmail.com>
2025-04-10 23:42:30 +0100

    update server configuration to serve static files and set view engine

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const connectDB = require("./config/db");
 const User = require("./models/User");
 const cors = require("cors");
+const path = require("path");
 
-const authRoutes = require('./routes/auth');
-const ticketRoutes = require('./routes/tickets');
-const adminRoutes = require('./routes/admin');
+const authRoutes = require("./routes/authRoutes");
+const ticketRoutes = require("./routes/ticketsRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+
 
 const app = express();
 
@@ -15,17 +17,30 @@ app.use(express.json());
 
 // Configure CORS to allow requests from http://localhost:4200
 const corsOptions = {
-  origin: 'http://localhost:4200',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  origin: "http://localhost:4200",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
 app.use(cors(corsOptions)); // Use cors with the options
 
+// Serve the static files
+app.use(express.static(path.join(__dirname, "public")));
+// Set the views engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use('/api/auth', authRoutes);
-app.use('/api/tickets', ticketRoutes);
-app.use('/api/admin', adminRoutes);
+// Routes pour servir les pages HTML
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
+const setupSwagger = require("./swagger/swaggerDocs");
+setupSwagger(app);
+
+// Routes 
+app.use("/api/auth", authRoutes);
+app.use("/api/tickets", ticketRoutes);
+app.use("/api/admin", adminRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
