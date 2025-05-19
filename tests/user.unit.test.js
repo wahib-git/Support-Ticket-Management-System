10a9400a3b03b6dc169d89b22fb36246020999d2
wahib bachoua <wahibbachoua95@gmail.com>
2025-05-19 16:51:40 +0100

    correction for unit tests

--- tests/user.unit.test.js
+++ tests/user.unit.test.js
@@ -1,19 +1,24 @@
-const User = require('../models/User');
+const mongoose = require("mongoose");
+const User = require("../models/User");
 
-describe('User model', () => {
-  it('should hash password before saving', async () => {
-    const user = new User({
-      name: 'Test',
-      email: 'test@example.com',
-      password: 'plainpassword',
-      role: 'agent',
-      specialization: 'Infrastructure informatique'
-    });
-    await user.save();
-    expect(user.password).not.toBe('plainpassword');
-  }, 15000); // 15 secondes
+beforeAll(async () => {
+  await mongoose.connect(process.env.MONGODB_URI_TEST, {});
 });
 
 afterAll(async () => {
-  await require('mongoose').connection.close();
+  await mongoose.connection.close();
+});
+
+describe("User model", () => {
+  it("should hash password before saving", async () => {
+    const user = new User({
+      name: "Test",
+      email: "test@example.com",
+      password: "plainpassword",
+      role: "agent",
+      specialization: "Infrastructure informatique",
+    });
+    await user.save();
+    expect(user.password).not.toBe("plainpassword");
+  }, 15000);
 });
