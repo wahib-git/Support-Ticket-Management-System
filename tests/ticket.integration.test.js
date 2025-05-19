a039441abe0956b1ff680cc9398d4686b6772045
wahib bachoua <wahibbachoua95@gmail.com>
2025-05-19 16:41:51 +0100

    correction in integration and  functional tests

--- tests/ticket.integration.test.js
+++ tests/ticket.integration.test.js
@@ -1,21 +1,24 @@
-const request = require('supertest');
-const app = require('../server');
-const mongoose = require('mongoose');
+const request = require("supertest");
+const app = require("../server");
+const mongoose = require("mongoose");
+const connectDB = require("../config/db");
 
-describe('Tickets API', () => {
-  afterAll(async () => {
-    await mongoose.connection.close();
-  });
+beforeAll(async () => {
+  await connectDB();
+});
 
-  it('should return 401 if not authenticated', async () => {
-    const res = await request(app)
-      .post('/api/tickets')
-      .send({
-        title: 'Test ticket',
-        description: 'desc',
-        category: 'Infrastructure informatique',
-        priority: 'urgent'
-      });
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+describe("Tickets API", () => {
+  it("should return 401 if not authenticated", async () => {
+    const res = await request(app).post("/api/tickets").send({
+      title: "Test ticket",
+      description: "desc",
+      category: "Infrastructure informatique",
+      priority: "urgent",
+    });
     expect(res.statusCode).toBe(401);
   });
 });
